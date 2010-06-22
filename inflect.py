2adf4257b7cbf2522f41ae7ff161814c55443253
Paul Dyson <pwdyson@yahoo.com>
2010-06-22 17:33:28 +1000

    remove s' removal on plural adjective test

--- inflect.py
+++ inflect.py
@@ -1085,7 +1085,7 @@ class engine:
             if self._pl_check_plurals_N(word2, word1):
                 return "p:p"
         if pl == self.pl or pl == self.pladj:
-            if self._pl_check_plurals_ADJ(word1, word2):
+            if self._pl_check_plurals_adj(word1, word2):
                 return "p:p"
         return False
 
@@ -1125,29 +1125,29 @@ class engine:
                 return True
         return False
 
-    def _pl_check_plurals_ADJ(self, word1, word2):
+    def _pl_check_plurals_adj(self, word1, word2):
 #VERSION: tuple in endswith requires python 2.5
         word1a = word1[:word1.rfind("'")] if word1.endswith(("'s","'")) else ''
         word2a = word2[:word2.rfind("'")] if word2.endswith(("'s","'")) else ''
         #TODO: BUG? report upstream. I don't think you should chop off the s'
-        word1b = word1[:-2] if word1.endswith("s'") else ''
-        word2b = word2[:-2] if word2.endswith("s'") else ''
+        #word1b = word1[:-2] if word1.endswith("s'") else ''
+        #word2b = word2[:-2] if word2.endswith("s'") else ''
 
         if word1a:
             if word2a and ( self._pl_check_plurals_N(word1a, word2a)
                             or self._pl_check_plurals_N(word2a, word1a) ):
                 return True
-            if word2b and ( self._pl_check_plurals_N(word1a, word2b)
-                            or self._pl_check_plurals_N(word2b, word1a) ):
-                return True
+        #    if word2b and ( self._pl_check_plurals_N(word1a, word2b)
+        #                    or self._pl_check_plurals_N(word2b, word1a) ):
+        #        return True
 
-        if word1b:
-            if word2a and ( self._pl_check_plurals_N(word1b, word2a)
-                            or self._pl_check_plurals_N(word2a, word1b) ):
-                return True
-            if word2b and ( self._pl_check_plurals_N(word1b, word2b)
-                            or self._pl_check_plurals_N(word2b, word1b) ):
-                return True
+        #if word1b:
+        #    if word2a and ( self._pl_check_plurals_N(word1b, word2a)
+        #                    or self._pl_check_plurals_N(word2a, word1b) ):
+        #        return True
+        #    if word2b and ( self._pl_check_plurals_N(word1b, word2b)
+        #                    or self._pl_check_plurals_N(word2b, word1b) ):
+        #        return True
         
         return False
 
