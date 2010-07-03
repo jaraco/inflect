76d733d83d42bd89cec1df9a8907e283561b2a33
Paul Dyson <pwdyson@yahoo.com>
2010-07-04 00:14:34 +1000

    change version to 0.1.7

--- inflect.py
+++ inflect.py
@@ -76,7 +76,7 @@ class BadRcFileError(Exception): pass
 
 __ver_major__ = 0
 __ver_minor__ = 1
-__ver_patch__ = 6
+__ver_patch__ = 7
 __ver_sub__ = ""
 __version__ = "%d.%d.%d%s" % (__ver_major__,__ver_minor__,
                               __ver_patch__,__ver_sub__)
