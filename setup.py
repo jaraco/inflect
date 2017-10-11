acbfb5de5232cd7e18bdc2cf593504c6a1bc7e6f
Hugo <hugovk@users.noreply.github.com>
2017-10-11 17:50:16 +0300

    Drop unsupported Python 3.3 https://en.wikipedia.org/wiki/CPython#Version_history

--- setup.py
+++ setup.py
@@ -29,9 +29,9 @@ setup(
     classifiers=[
         'Development Status :: 3 - Alpha',
         'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.3',
         'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
         'Intended Audience :: Developers',
