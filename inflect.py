aadec6ca58d277f1b0ca3a10f20a50beca76cbf4
Jason R. Coombs <jaraco@jaraco.com>
2018-05-08 20:38:04 +0100

    Bump to 0.3.0

--- inflect.py
+++ inflect.py
@@ -95,8 +95,8 @@ class BadGenderError(Exception):
     pass
 
 __ver_major__ = 0
-__ver_minor__ = 2
-__ver_patch__ = 5
+__ver_minor__ = 3
+__ver_patch__ = 0
 __ver_sub__ = ""
 __version__ = "%d.%d.%d%s" % (__ver_major__, __ver_minor__,
                               __ver_patch__, __ver_sub__)
