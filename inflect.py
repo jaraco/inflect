e3fb7a37453a0bf9b5f99ff8182747cdc833b232
Jason R. Coombs <jaraco@jaraco.com>
2018-05-12 09:39:02 -0400

    Simply declare the version

--- inflect.py
+++ inflect.py
@@ -95,12 +95,7 @@ class BadGenderError(Exception):
     pass
 
 
-__ver_major__ = 0
-__ver_minor__ = 3
-__ver_patch__ = 1
-__ver_sub__ = ""
-__version__ = "%d.%d.%d%s" % (__ver_major__, __ver_minor__,
-                              __ver_patch__, __ver_sub__)
+__version__ = "0.3.1"
 
 
 STDOUT_ON = False
