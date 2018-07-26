ec6635afa49ce92af94c0a5dfccace2e63c642b7
Hugo <hugovk@users.noreply.github.com>
2018-07-26 14:47:29 +0300

    Update versions

--- setup.py
+++ setup.py
@@ -36,6 +36,7 @@ setup(
         'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
         'Intended Audience :: Developers',
         'License :: OSI Approved :: GNU Affero General Public License v3',
         'Natural Language :: English',
@@ -43,7 +44,7 @@ setup(
         'Topic :: Software Development :: Libraries :: Python Modules',
         'Topic :: Text Processing :: Linguistic',
     ],
-    python_requires=">=2.7,!=3.2",
+    python_requires='>=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*',
     setup_requires=[
         'setuptools_scm',
     ],
