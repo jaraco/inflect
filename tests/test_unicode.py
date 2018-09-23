2dd80faec2f73cac32e1359549001dd893064e31
Niels Mündler <n.muendler@tum.de>
2018-09-23 21:50:10 +0200

    Add unicode test line

---
+++ tests/test_unicode.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+from __future__ import unicode_literals
+
+import unittest
+import inflect
+
+
+class TestUnicode(unittest.TestCase):
+    def test_unicode(self):
+        engine = inflect.engine()
+        # Unicode compatability test
+        unicode_test_cases = {
+            'cliché': 'clichés'
+        }
+        for singular, plural in unicode_test_cases.items():
+            self.assertEqual(plural, engine.plural(singular))
