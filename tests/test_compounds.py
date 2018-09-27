c0d198f117968dc824e45d107deb447b3b12546c
Niels Mündler <n.muendler@tum.de>
2018-09-27 12:52:05 +0200

    Capitalize Celsius

--- tests/test_compounds.py
+++ tests/test_compounds.py
@@ -23,7 +23,7 @@ def test_compound_4():
 
 def test_unit_handling_degree():
     test_cases = {
-        'degree celsius': 'degrees celsius',
+        'degree Celsius': 'degrees Celsius',
         'degree fahrenheit': 'degrees fahrenheit',
         'degree rankine': 'degrees rankine'
     }
@@ -34,14 +34,14 @@ def test_unit_handling_degree():
 def test_unit_handling_fractional():
     test_cases = {
         'pound per square inch': 'pounds per square inch',
-        'pound-force per square inch': 'pounds-force per square inch',
         'metre per second': 'metres per second',
         'kilometre per hour': 'kilometres per hour',
         'cubic metre per second': 'cubic metres per second',
         'dollar a year': 'dollars a year',
         # Correct pluralization of denominator
         'foot per square second': 'feet per square second',
-        'mother-in-law per lifetime': 'mothers-in-law per lifetime'
+        'mother-in-law per lifetime': 'mothers-in-law per lifetime',
+        'pound-force per square inch': 'pounds-force per square inch',
     }
     for singular, plural in test_cases.items():
         eq_(p.plural(singular), plural)
@@ -51,7 +51,7 @@ def test_unit_handling_combined():
     test_cases = {
         # Heat transfer coefficient unit
         'watt per square meter degree celsius': 'watts per square meter degree celsius',
-        'degree celsius per hour': 'degrees celsius per hour'
+        'degree Celsius per hour': 'degrees Celsius per hour'
     }
     for singular, plural in test_cases.items():
         eq_(p.plural(singular), plural)
