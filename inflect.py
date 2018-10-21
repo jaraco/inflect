1e61112c1e86c563bc3aa965b35d47bef81b3883
Tyr Mactire <tyr@pettingzoo.co>
2018-10-20 20:24:03 -0700

    add irregular plural: tacos

--- inflect.py
+++ inflect.py
@@ -1974,6 +1974,8 @@ class engine:
             return word[:-5] + 'teeth'
         if lowerword[-4:] == 'foot':
             return word[:-4] + 'feet'
+        if lowerword[-4:] == 'taco':
+            return word[:-5] + 'tacos'
 
         if lowerword == 'die':
             return 'dice'
