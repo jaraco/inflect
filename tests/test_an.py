e2e11f08196d9de32128b09af5925ec529a57a95
Jason R. Coombs <jaraco@jaraco.com>
2019-11-07 07:07:52 -0800

    Replace eq_ with simple assertions. Ref #90.

--- tests/test_an.py
+++ tests/test_an.py
@@ -1,22 +1,20 @@
-from nose.tools import eq_
-
 import inflect
 
 
 def test_an():
     p = inflect.engine()
 
-    eq_(p.an("cat"), "a cat", msg="a cat")
-    eq_(p.an("ant"), "an ant", msg="an ant")
-    eq_(p.an("a"), "an a", msg="an a")
-    eq_(p.an("b"), "a b", msg="a b")
-    eq_(p.an("honest cat"), "an honest cat", msg="an honest")
-    eq_(p.an("dishonest cat"), "a dishonest cat", msg="a dishonest")
-    eq_(p.an("Honolulu sunset"), "a Honolulu sunset", msg="a Honolulu")
-    eq_(p.an("mpeg"), "an mpeg", msg="an mpeg")
-    eq_(p.an("onetime holiday"), "a onetime holiday", msg="a onetime")
-    eq_(p.an("Ugandan person"), "a Ugandan person", msg="a Ugandan")
-    eq_(p.an("Ukranian person"), "a Ukranian person", msg="a Ukranian")
-    eq_(p.an("Unabomber"), "a Unabomber", msg="a Unabomber")
-    eq_(p.an("unanimous decision"), "a unanimous decision", msg="a unanimous")
-    eq_(p.an("US farmer"), "a US farmer", msg="a US")
+    assert p.an("cat") == "a cat"
+    assert p.an("ant") == "an ant"
+    assert p.an("a") == "an a"
+    assert p.an("b") == "a b"
+    assert p.an("honest cat") == "an honest cat"
+    assert p.an("dishonest cat") == "a dishonest cat"
+    assert p.an("Honolulu sunset") == "a Honolulu sunset"
+    assert p.an("mpeg") == "an mpeg"
+    assert p.an("onetime holiday") == "a onetime holiday"
+    assert p.an("Ugandan person") == "a Ugandan person"
+    assert p.an("Ukranian person") == "a Ukranian person"
+    assert p.an("Unabomber") == "a Unabomber"
+    assert p.an("unanimous decision") == "a unanimous decision"
+    assert p.an("US farmer") == "a US farmer"
