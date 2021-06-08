c0f3e473fd62c40a4f982c7be780c223b390ae7e
David Lord <davidism@gmail.com>
2021-06-07 21:48:25 -0700

    apply black formatting

--- tests/test_unicode.py
+++ tests/test_unicode.py
@@ -3,10 +3,10 @@ import inflect
 
 
 class TestUnicode(unittest.TestCase):
-    """ Unicode compatibility test cases """
+    """Unicode compatibility test cases"""
 
     def test_unicode_plural(self):
-        """ Unicode compatibility test cases for plural """
+        """Unicode compatibility test cases for plural"""
         engine = inflect.engine()
         unicode_test_cases = {"cliché": "clichés", "ångström": "ångströms"}
         for singular, plural in unicode_test_cases.items():
