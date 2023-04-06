26bd1a33e03892aa3e2a6f3d9181a9aff6411b33
Jason R. Coombs <jaraco@jaraco.com>
2023-04-05 21:32:55 -0400

    Inline words parameterization, as it only has two words.

--- tests/test_pl_si.py
+++ tests/test_pl_si.py
@@ -1,15 +1,6 @@
-import pathlib
-
 import pytest
 
 import inflect
-
-
-FNAME = "tests/words.txt"
-
-
-def suitable_for_pl_si(word):
-    return word and not word.endswith("'s")
 
 
 @pytest.fixture(params=[False, True], ids=['classical off', 'classical on'])
@@ -17,16 +8,7 @@ def classical(request):
     return request.param
 
 
-def suitable_words():
-    words = pathlib.Path(FNAME).read_text(encoding='utf-8').splitlines()
-    return filter(suitable_for_pl_si, words)
-
-
-@pytest.fixture(params=suitable_words())
-def word(request):
-    return request.param
-
-
+@pytest.mark.parametrize("word", ['Times', 'Jones'])
 def test_pl_si(classical, word):
     p = inflect.engine()
     p.classical(all=classical)
