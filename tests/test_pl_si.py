a25060bfbecac5e9aaf95b08dfd975939ffab368
Jason R. Coombs <jaraco@jaraco.com>
2023-04-05 21:30:34 -0400

    Parameterize 'words' in test_pl_si

--- tests/test_pl_si.py
+++ tests/test_pl_si.py
@@ -17,21 +17,17 @@ def classical(request):
     return request.param
 
 
-def test_pl_si(classical):
+def suitable_words():
+    words = pathlib.Path(FNAME).read_text(encoding='utf-8').splitlines()
+    return filter(suitable_for_pl_si, words)
+
+
+@pytest.fixture(params=suitable_words())
+def word(request):
+    return request.param
+
+
+def test_pl_si(classical, word):
     p = inflect.engine()
     p.classical(all=classical)
-    words = pathlib.Path(FNAME).read_text(encoding='utf-8').splitlines()
-    for word in filter(suitable_for_pl_si, words):
-        check_pl_si(p, word)
-
-
-def check_pl_si(p, word):
-    if p.singular_noun(p.plural_noun(word, 2), 1) != word:
-        f = open("badsi.txt", "a", encoding='utf-8')
-        f.write(
-            "{} {} {}\n".format(
-                word, p.plural_noun(word, 2), p.singular_noun(p.plural_noun(word, 2), 1)
-            )
-        )
-        f.close()
-        assert p.singular_noun(p.plural_noun(word, 2), 1) == word
+    assert p.singular_noun(p.plural_noun(word, 2), 1) == word
