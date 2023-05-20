d09c9a3fd9cbb7b36a6fb3cffb9395ccda955829
Stanislav Zmiev <szmiev2000@gmail.com>
2023-05-20 17:32:14 +0400

    Improve typehints for singular noun

--- inflect/__init__.py
+++ inflect/__init__.py
@@ -70,6 +70,7 @@ from typing import (
     cast,
     Any,
 )
+from typing_extensions import Literal
 from numbers import Number
 
 
@@ -2558,7 +2559,7 @@ class engine:
         text: Word,
         count: Optional[Union[int, str, Any]] = None,
         gender: Optional[str] = None,
-    ) -> Union[str, bool]:
+    ) -> Union[str, Literal[False]]:
         """
         Return the singular of text, where text is a plural noun.
 
